0fd28e725c08f272f805e54151f4db67923e8082
katzhukova <katzhukova@gmail.com>
2022-04-17 20:09:09 +0300

    Выполнила задание 3

---
+++ HomeworkTheme08ConsoleAppTask03/HashSetRepository.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeworkTheme08ConsoleAppTask03
+{
+    class HashSetRepository
+    {
+        private HashSet<int> SetInt { get; set; }
+
+        public HashSetRepository()
+        {
+            SetInt = new HashSet<int>();
+        }
+
+        public HashSet<int> Add (int number)
+        {
+            SetInt.Add(number);
+            return SetInt;
+        }
+
+        public void ChekingRepetitions(int number)
+        {
+            if (SetInt.Contains(number))
+            {
+                Console.WriteLine($"\nЧисло {number} уже вводилось ранее.");
+            }
+            else
+            {
+                Console.WriteLine($"\nЧисло {number} успешно сохранено.");
+            }
+        }
+
+        public void PrintHashSet ()
+        {
+            foreach (var e in SetInt)
+            {
+                Console.Write($"{e} ");
+            }
+        }
+    }
+}
---
+++ HomeworkTheme08ConsoleAppTask03/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeworkTheme08ConsoleAppTask03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var hashSetRepository = new HashSetRepository();
+
+            Console.WriteLine("Задание 3. Проверка повторов:");
+            Console.WriteLine("Признаком того, что пользователь закончил вводить числа, является ввод пустой строки...");
+            while (true)
+            {
+                Console.WriteLine("Введите число:");
+                string userNumber = Console.ReadLine();
+                if (int.TryParse(userNumber, out int result))
+                {
+                    hashSetRepository.ChekingRepetitions(result);
+                    hashSetRepository.Add(result);
+                }
+                else
+                {
+                    break;
+                }
+            }
+            Console.WriteLine("У вас получилось такое множество:");
+            hashSetRepository.PrintHashSet();
+
+            Console.ReadKey();
+        }
+    }
+}
